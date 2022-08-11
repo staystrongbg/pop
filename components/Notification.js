c0f030780772277c106b982ff7299fd9f5e05baa
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-08-11 18:20:50 +0200

    Add files via upload

--- components/Notification.js
+++ components/Notification.js
@@ -1,11 +1,20 @@
 import styles from '../styles/Notification.module.scss';
 const Notification = () => {
+  const removeNotification = (e) => {
+    e.target.offsetParent.remove();
+  };
   return (
     <div className={styles.notification}>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, soluta.
         Ipsum voluptates quasi nam. Necessitatibus ab labore.
-        <span style={{ marginLeft: '10px' }}>&#10006;</span>
+        <span
+          style={{ marginLeft: '10px', cursor: 'pointer' }}
+          onClick={(e) => removeNotification(e)}
+          title='click to close'
+        >
+          &#10006;
+        </span>
       </p>
     </div>
   );
